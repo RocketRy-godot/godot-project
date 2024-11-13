49e85cacf5654474583fa3a429f387ddaf0dc749
losingpaku <tajaffar30143@gmail.com>
2024-11-13 23:47:49 +0530

    added collectables, dynamic amera movements and such.

---
+++ scenes/shards_of_lumaria.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var game_manager: Node = %"Game Manager"
+
+
+
+
+
+func _on_body_entered(body):
+	if (body.name == "CharacterBody2D"):
+		queue_free()
+		game_manager.add_point()
