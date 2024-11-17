f285a9f9c06c82e747ae616f97ab3b24529f2815
RocketRy-godot <mraihaan3@gmail.com>
2024-11-18 00:28:30 +0530

    BIG PAUSE GUI!!!! SICKKKKK 10/10
    balls

---
+++ pause.gd
@@ -0,0 +1,33 @@
+extends Control
+
+
+
+func resume():
+	get_tree().paused = false
+	$CanvasLayer/pauseanimation.play_backwards('blur')
+
+func pause():
+	get_tree().paused = true
+	$CanvasLayer/pauseanimation.play('blur')
+
+func testEsc():
+	if Input.is_action_just_pressed('esc') and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed('esc') and get_tree().paused == true:
+		resume()
+	
+
+func _on_resume_pressed():
+	resume()
+	
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
+	resume()
+	
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
